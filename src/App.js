73dec5036487c1eb94ffd83c60f1a57a4377ca83
1serena1 <serenabaza2@gmail.com>
2022-11-19 15:11:25 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,43 @@
+import React, { Component } from "react";
+//import { Route, Redirect, Switch } from "react-router-dom";
+import Movies from "./components/movies";
+import MovieForm from "./components/movieForm";
+import Customers from "./components/customers";
+import Rentals from "./components/rentals";
+import NotFound from "./components/notFound";
+import NavBar from "./components/navBar";
+import LoginForm from "./components/loginForm";
+import RegisterForm from "./components/registerForm";
+import "./App.css";
+import axios from 'axios';
+import { BrowserRouter as Router, Route, NavLink ,Redirect, Switch } from "react-router-dom";
+axios.defaults.baseURL = "http://127.0.0.1:8000/";
+
+class App extends Component {
+  render() {
+    let navLink = (
+    <NavLink exact to="/register" activeClassName="activeLink" className="registerForm">
+          Sign Up
+        </NavLink>);
+  return (
+    <React.Fragment>
+      <NavBar />
+      <main className="container">
+        <Switch>
+          <Route exact path="/register" component={RegisterForm} />
+          <Route path="/login" component={LoginForm} />
+          <Route path="/movies/:id" component={MovieForm} />
+          <Route path="/movies" component={Movies} />
+          <Route path="/customers" component={Customers} />
+          <Route path="/rentals" component={Rentals} />
+          <Route path="/not-found" component={NotFound} />
+          <Redirect from="/" exact to="/movies" />
+          <Redirect to="/not-found" />
+        </Switch>
+      </main>
+    </React.Fragment>
+  );
+}
+}
+
+export default App;
---
+++ src/components/registerForm.jsx
@@ -0,0 +1,242 @@
+import React, { Component } from "react";
+import Joi from "joi-browser";
+//import Form from "./common/form";
+import {Link, useHistory} from 'react-router-dom';
+import { Button, Form, FormGroup, Label, Input } from "reactstrap";
+import axios from "axios";
+
+const options = {
+  headers: {
+       //'Authorization': this.state.Authorization,
+       'content-type': 'application/json',
+       'Accept':'application/json',
+       'xsrfHeaderName': 'X-CSRF-TOKEN',
+       },
+  data: {gcm_id: 1}
+  };
+export default class RegisterForm extends Component {
+
+  userData;
+  constructor(props) {
+    super(props);
+    this.state = {
+      signupData: {
+        name: "",
+        dateOfBirth: "",
+        email: "",
+        number: "",
+        county: "",
+        city: "",
+        address: "",
+        password: "",
+        pass_conf: "",
+        //isLoading: "",
+      },
+      msg: "",
+    };
+  }
+  onChangehandler = (e, key) => {
+    const { signupData } = this.state;
+    signupData[e.target.name] = e.target.value;
+    this.setState({ signupData });
+  };
+  
+  onSubmitHandler = (e) => {
+    e.preventDefault();
+    this.setState({ isLoading: true });
+    axios
+      .post("http://127.0.0.1:8000/api/register", this.state.signupData,  options)
+      .then((response) => {
+        this.setState({ isLoading: false });
+        if (response.data.status === 200) {
+          this.setState({
+            msg: response.data.message,
+            signupData: {
+              name: "",
+              dateOfBirth: "",
+              email: "",
+              number: "",
+              county: "",
+              city: "",
+              address: "",
+              password: "",
+              pass_conf: "",
+            },
+          });
+          setTimeout(() => {
+            this.setState({ msg: "" });
+          }, 2000);
+        }
+
+        if (response.data.status === "failed") {
+          this.setState({ msg: response.data.message });
+          setTimeout(() => {
+            this.setState({ msg: "" });
+          }, 2000);
+        }
+      });
+  };
+
+  render() {
+    const isLoading = this.state.isLoading;
+    return (
+      <div>
+        <Form className="containers shadow">
+          <FormGroup>
+            <Label for="name">Name</Label>
+            <Input
+              type="name"
+              name="name"
+              placeholder="Enter name"
+              value={this.state.signupData.name}
+              onChange={this.onChangehandler}
+            />
+          </FormGroup>
+          <FormGroup>
+            <Label for="dateofbirth"> date of birth</Label>
+            <Input
+              type="dateofbirth"
+              name="dateOfBirth"
+              placeholder="Enter date of birth"
+              value={this.state.signupData.dateOfBirth}
+              onChange={this.onChangehandler}
+            />
+          </FormGroup>
+          <FormGroup>
+            <Label for="email">Email id</Label>
+            <Input
+              type="email"
+              name="email"
+              placeholder="Enter email"
+              value={this.state.signupData.email}
+              onChange={this.onChangehandler}
+            />
+          </FormGroup>
+          <FormGroup>
+            <Label for="number">Phone Number</Label>
+            <Input
+              type="number"
+              name="number"
+              placeholder="Enter phone number"
+              value={this.state.signupData.number}
+              onChange={this.onChangehandler}
+            />
+          </FormGroup>
+          <FormGroup>
+            <Label for="county">county</Label>
+            <Input
+              type="county"
+              name="county"
+              placeholder="Enter county"
+              value={this.state.signupData.county}
+              onChange={this.onChangehandler}
+            />
+          </FormGroup>
+          <FormGroup>
+            <Label for="city">city</Label>
+            <Input
+              type="city"
+              name="city"
+              placeholder="Enter city"
+              value={this.state.signupData.city}
+              onChange={this.onChangehandler}
+            />
+          </FormGroup>
+          <FormGroup>
+            <Label for="address">address</Label>
+            <Input
+              type="address"
+              name="address"
+              placeholder="Enter address"
+              value={this.state.signupData.address}
+              onChange={this.onChangehandler}
+            />
+          </FormGroup>
+          <FormGroup>
+            <Label for="password">Password</Label>
+            <Input
+              type="password"
+              name="password"
+              placeholder="Enter password"
+              value={this.state.signupData.password}
+              onChange={this.onChangehandler}
+            />
+          </FormGroup>
+          <FormGroup>
+            <Label for="pass_conf">PasswordConfirmation</Label>
+            <Input
+              type="pass_conf"
+              name="pass_conf"
+              placeholder="Enter password confirmation"
+              value={this.state.signupData.pass_conf}
+              onChange={this.onChangehandler}
+            />
+          </FormGroup>
+          <p className="text-white">{this.state.msg}</p>
+          <Button
+            className="text-center mb-4"
+            color="success"
+            onClick={this.onSubmitHandler}
+          >
+            Sign Up
+            {isLoading ? (
+              <span
+                className="spinner-border spinner-border-sm ml-5"
+                role="status"
+                aria-hidden="true"
+              ></span>
+            ) : (
+              <span></span>
+            )}
+          </Button>
+          <Link to="/register" className="text-white ml-5">I'm already member</Link>
+        </Form>
+      </div>
+    );
+  }
+
+
+  // state = {
+  //   data: { username: "", password: "", name: "", newname:"" },
+  //   errors: {}
+  // };
+
+  // schema = {
+  //   username: Joi.string()
+  //     .required()
+  //     .email()
+  //     .label("Username"),
+  //   password: Joi.string()
+  //     .required()
+  //     .min(5)
+  //     .label("Password"),
+  //   name: Joi.string()
+  //     .required()
+  //     .label("Name"),
+  //   newname:Joi.string()
+  //     .required()
+  //     .label("New Name")
+  // };
+
+  // doSubmit = () => {
+  //   // Call the server
+  //   console.log("Submitted");
+  // };
+
+  // render() {
+  //   return (
+  //     <div>
+  //       <h1>Register</h1>
+  //       <form onSubmit={this.handleSubmit}>
+  //         {this.renderInput("username", "Username")}
+  //         {this.renderInput("password", "Password", "password")}
+  //         {this.renderInput("name", "Name")}
+  //         {this.renderInput("newname", "New Name")}
+  //         {this.renderButton("Register")}
+  //       </form>
+  //     </div>
+  //   );
+  // }
+}
+
+//export default RegisterForm;
